0ab1072364c30057b5ac2a5eb25f34ba80273cd4
makaempffer <ma.kaempffer@duocuc.cl>
2023-05-03 17:14:01 -0400

    Navbar + Menu start

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,13 @@
 
 export default function Navbar() {
     return(
-        <div class="h-10 w-screen bg-zinc-900 border-white border">
-            <div class="flex content-even justify-start p-2 text-slate-100">
-                <a class="hover:underline decoration-blue-400" href="/">Home</a>
-                <a class="hover:underline decoration-blue-400" href="/about">About</a>
+        <div class="h-14 w-screen bg-zinc-900 border-white border">
+            <div class="flex content-even justify-start p-2 text-slate-100 content-center">
+            <a href="/" class="text-white bg-gradient-to-r rounded p-1 from-indigo-500 via-purple-500 to-pink-500 text-xl duration-500 hover:underline decoration-cyan-500">DevSpace</a>
+                <a class="hover:underline decoration-blue-400 mt-2" href="/">Home</a>
+                <a class="hover:underline decoration-blue-400 mt-2" href="/about">About</a>
             </div>
+            
         </div>
     )
 }
